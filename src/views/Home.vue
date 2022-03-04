40c39bef84d2747e9f71100308e8b17f206e4dd0
Adrien Gonçalves <adrien@ignition-program.com>
2022-03-04 15:46:30 +0100

    end

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Event Aie</h1>
+  <div class="events">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <EventCard v-for="event in events" :event="event" :key="event.id" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 3444320,
+          category: "plop category",
+          title: "title plop",
+          description: "description plop",
+          location: "location plop",
+          date: "April 1, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "organizer plop"
+        },
+        {
+          id: 3444321,
+          category: "plop2 category",
+          title: "title plop2",
+          description: "description plop2",
+          location: "location plop2",
+          date: "April 1, 2022",
+          time: "12:30",
+          petsAllowed: true,
+          organizer: "organizer plop2"
+        },
+        {
+          id: 3444322,
+          category: "plop3 category",
+          title: "title plop3",
+          description: "description plop3",
+          location: "location plop3",
+          date: "April 1, 2022",
+          time: "13:30",
+          petsAllowed: true,
+          organizer: "organizer plop3"
+        }
+      ]
+    };
   }
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
